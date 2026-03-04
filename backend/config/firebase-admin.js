7cca08c1cbeb6b6f3ade431a1b3937b7e7bbefbb
Shagun Mohta <85337809+Shagun20@users.noreply.github.com>
2026-03-04 23:26:16 +0530

    Update firebase-admin.js

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -5,16 +5,14 @@ import { dirname, join } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: join(__dirname, "../credentials.env") });
 
-// console.log("DB URL:", process.env.FIREBASE_DATABASE_URL);
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 

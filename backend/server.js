24cc084ace6eb2385c67f06b5e17b0a8f2f1a823
Shagun Mohta <85337809+Shagun20@users.noreply.github.com>
2026-03-04 23:21:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const lc = new LeetCode();
 const app = express();
 const PORT = 3000;
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://codecolab-mlt4q71f8-shagun20s-projects.vercel.app' || 'http://localhost:5173' }));
 app.use(express.json());
 app.use('', router);
 
